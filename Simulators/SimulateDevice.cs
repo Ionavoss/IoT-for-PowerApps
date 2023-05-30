b8b57529f2bb32de2d6cb25255c5d8c77a4857b0
Iona <46789417+Ionavoss@users.noreply.github.com>
2023-05-30 16:12:42 +0200

    Create SimulateDevice.cs

---
+++ Simulators/SimulateDevice.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+
+
+class Program
+{
+    private static DeviceClient deviceClient;
+    private static readonly string connectionString = "Your-IoT-Device-Connection-String"; //"i.e. HostName=iothubname.azure-devices.net;DeviceId=device1;SharedAccessKey=testSASle99=ekf";
+
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Simulated device\n");
+        deviceClient = DeviceClient.CreateFromConnectionString(connectionString, TransportType.Mqtt);
+
+        await deviceClient.OpenAsync();
+
+        Console.WriteLine("Device connected to IoT Hub");
+
+        await deviceClient.SetReceiveMessageHandlerAsync(HandleReceivedMessageAsync, deviceClient);
+
+        await SendDeviceToCloudMessagesAsync();
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadLine();
+
+        await deviceClient.CloseAsync();
+    }
+
+    private static async Task<MessageResponse> HandleReceivedMessageAsync(Message message, object userContext)
+    {
+        string messageData = Encoding.UTF8.GetString(message.GetBytes());
+        Console.WriteLine($"Received message: {messageData}");
+
+        // Complete the message (mark it as processed) so that it is not received again
+        await deviceClient.CompleteAsync(message);
+
+        return MessageResponse.Completed;
+    }
+
+    private static async Task SendDeviceToCloudMessagesAsync()
+    {
+        Random rand = new Random();
+
+        while (true)
+        {
+            int temperature = rand.Next(20, 30);
+            int humidity = rand.Next(60, 80);
+
+            string messageString = $"{{\"temperature\":{temperature},\"humidity\":{humidity}}}";
+            byte[] messageBytes = Encoding.UTF8.GetBytes(messageString);
+            Message message = new Message(messageBytes);
+
+            Console.WriteLine($"Sending message: {messageString}");
+
+            await deviceClient.SendEventAsync(message);
+
+            await Task.Delay(5000);
+        }
+    }
+}
